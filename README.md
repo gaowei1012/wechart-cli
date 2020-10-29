e057b285b79d2415e37d0cbf65721727d6179fdd
gaowei1012 <mingwei2222@126.com>
2020-10-29 10:25:44 +0800

    添加代码监听编译

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 小程序自动化打包编译平台
+
+运行
+
+```base
+cd wx && npm install 
+
+npm run build /// 生成打包后的代码，直接可以发不上线
+
+npm run watch /// 开发中使用，监听文件动态修改
+```
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,27 @@
 const path = require('path')
+const webpack = require('webpack')
 const CoypWebpackPlugins = require('copy-webpack-plugin')
 const EntryExtractPlugin = require('./entry-extract-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 const ABSOLUTE_PATH = process.cwd()
+const deuggable = process.env.BUILD_TYPE !== 'release' 
 
 module.exports = {
+    watch: true,
+    watchOptions: {
+        //默认为空，不监听的文件或者文件夹，支持正册匹配
+        ignored:/node_modules/,
+        //监听到变化发生后会等到300ms再去执行，默认300ms
+        aggregateTimeout: 300,
+        //判断文件是否发生变化是通过不停轮循系统指定文件有没有变化实现的
+        //默认每秒问1000次
+        poll: 1000
+    },
+    mode: deuggable ? 'none' : 'production',
+    devtool: deuggable ? 'inline-source-map' : 'source-map',
     context: path.resolve(ABSOLUTE_PATH, 'src'),
-
     entry: {
         app: './app.js',
         // 'pages/home/index': './pages/home/index.js',
@@ -85,9 +98,27 @@ module.exports = {
             dry: true,
             verbose: true,
         }),
-        
+        new webpack.EnvironmentPlugin({
+            NODE_ENV: JSON.stringify(process.env.NODE_ENV) || 'developement',
+            BUILD_TYPE: JSON.stringify(process.env.BUILD_TYPE) || 'debug'
+        }),
     ],
     optimization: {
-        minimize: true,
+        splitChunks: {
+            chunks: 'all',
+            name: 'common',
+            minChunks: 2,
+            minSize: 0
+        },
+        /// runtime.js plugin
+        runtimeChunk: {
+            name: 'runtime'
+        },
+        minimize: true
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9090
     }
 }
